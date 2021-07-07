b67bfa66a6ac13fbbc23c470feaa9392bd52070b
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-07-07 15:32:51 +0300

    add Projects files 3

---
+++ Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace FootballTeam
+{
+    public class Player
+    {
+        private const int MinStatValue = 0;
+        private const int MaxStatValue = 100;
+
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        public Player(
+            string name,
+            int endurance,
+            int sprint,
+            int dribble,
+            int passing,
+            int shooting)
+        {
+            this.Name = name;
+            this.Endurance = endurance;
+            this.Sprint = sprint;
+            this.Dribble = dribble;
+            this.Passing = passing;
+            this.Shooting = shooting;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Endurance
+        {
+            get => this.endurance;
+            private set
+            {              
+                ValidateStatValue(value, nameof(Endurance));
+                this.endurance = value;
+            }
+        }
+
+        public int Sprint
+        {
+            get => this.sprint;
+            private set
+            {
+                ValidateStatValue(value, nameof(Sprint));
+                this.sprint = value;
+            }
+        }
+
+        public int Dribble
+        {
+            get => this.dribble;
+            private set
+            {
+                ValidateStatValue(value, nameof(Dribble));
+                this.dribble = value;
+            }
+        }
+
+        public int Passing
+        {
+            get => this.passing;
+            private set
+            {
+                ValidateStatValue(value, nameof(Passing));
+                this.passing = value;
+            }            
+        }
+
+        public int Shooting
+        {
+            get => this.shooting;
+            private set
+            {
+                ValidateStatValue(value, nameof(Shooting));
+                this.shooting = value;
+            }
+        }
+
+        public int GetSkillLevel => SkillLevel();
+
+        private int SkillLevel() 
+        {
+            double countPlayerSkills = 5.0;
+            return (int)Math.Round(
+            (this.Endurance + this.Sprint + this.Dribble + this.Passing + this.Shooting)
+                 / countPlayerSkills );
+        }
+        private void ValidateStatValue(int value, string propertyName)
+        {
+            if (value < MinStatValue || value > MaxStatValue)
+            {
+                throw new ArgumentException(
+               $"{propertyName} should be between {MinStatValue} and {MaxStatValue}.");
+            }
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeam
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Player>players;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+
+        public string Name 
+        {
+            get => this.name;
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                this.name = value;
+            } 
+        }
+
+        public double GetTeamRating => this.TeamRating();
+        public void AddPlayer(Player player) 
+        {
+            this.players.Add(player);
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            Player player = players.FirstOrDefault(pl => pl.Name == playerName);
+
+            if (player is null)
+            {
+               throw new InvalidOperationException(
+                   $"Player {playerName} is not in {this.Name} team.");
+            }
+
+            players.Remove(player);
+        }
+
+        private int TeamRating()
+        {
+            if (players.Count == 0)
+            {
+                return 0;
+            }
+
+            var rating = players.Sum(pl => pl.GetSkillLevel);
+            
+            var teamRating = rating / players.Count;
+            return teamRating; // TeamRating  = SumPlayersRating / countPlayers
+
+            //int resRating = default;
+            //foreach (var player in players)
+            //{
+            //    rating += player.GetSkillLevel;
+            //}
+        }
+    }
+}
